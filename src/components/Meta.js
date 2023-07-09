bf919f803959b4e0a6ae42e1ff497270d50abbb5
ngecu <devngecu@gmail.com>
2023-07-10 01:46:58 +0300

    meta data changes

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -7,14 +7,24 @@ const Meta = ({ title, description, keywords }) => {
       <title>{title}</title>
       <meta name='description' content={description} />
       <meta name='keyword' content={keywords} />
+
+      <meta property="og:type" content={type} />
+<meta property="og:title" content={title} />
+<meta property="og:description" content={description} />
+
+      <meta name="twitter:creator" content={name} />
+<meta name="twitter:card" content={type} />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+
     </Helmet>
   )
 }
 
 Meta.defaultProps = {
-  title: 'Drink&Chill',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'DrinksAndChill',
+  description: 'We sell the best alcoholic products for cheap',
+  keywords: 'drinsandchill,drinks and chill,drink&chill,drink,chill,drink muranga,drink chill muranga,drinkandchill,alcohol, murang"a wines and spirit, muranga drink and chill,beer,wines and spirit',
 }
 
 export default Meta
